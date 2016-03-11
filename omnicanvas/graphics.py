08d26002ab3f3708822d385d73eea9319476b19e
Sam Ireland <sam.ireland.uk@gmail.com>
2016-03-11 20:41:13 +0000

    Outline basic graphic types

---
+++ omnicanvas/graphics.py
@@ -0,0 +1,37 @@
+class GenericRectangle:
+    pass
+
+
+
+class Line:
+    pass
+
+
+
+class Path:
+    pass
+
+
+
+class Rectangle(GenericRectangle):
+    pass
+
+
+
+class Polygon:
+    pass
+
+
+
+class Oval(GenericRectangle):
+    pass
+
+
+
+class Arc(GenericRectangle):
+    pass
+
+
+
+class Text(GenericRectangle):
+    pass
