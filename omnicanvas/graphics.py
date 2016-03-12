440de85f44f9f49e780d7342fb41dc072383e40b
Sam Ireland <sam.ireland.uk@gmail.com>
2016-03-12 00:35:03 +0000

    Flesh out Line, Path and Rectangle objects, with Unit Tests

--- omnicanvas/graphics.py
+++ omnicanvas/graphics.py
@@ -10,47 +10,77 @@ class GenericBox:
 
 class GenericLine:
 
-    def __init__(self, line_style, line_width, line_color):
+    def __init__(self, line_style="-", line_width=1, line_color="#000000"):
         self.line_style = line_style
         self.line_width = line_width
         self.line_color = line_color
 
 
 
-class GenericShape:
+class GenericShape(GenericLine):
 
-    def __init__(self, fill_color, opacity):
+    def __init__(self, fill_color=None, opacity=1, **kwargs):
         self.fill_color = fill_color
         self.opacity = opacity
+        GenericLine.__init__(self, **kwargs)
 
 
 
 class Line(GenericLine):
-    pass
+
+    def __init__(self, x1, y1, x2, y2, **kwargs):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        GenericLine.__init__(self, **kwargs)
+
+
+    def __repr__(self):
+        return "<Line object: (%i,%i) to (%i,%i)>" % (
+         self.x1, self.y1, self.x2, self.y2
+        )
 
 
 
 class Path(GenericLine):
+
+    def __init__(self, *points, **kwargs):
+        assert len(points) % 2 == 0, "An even number of points must be supplied"
+        self.points = list(zip(points[::2], points[1::2]))
+        GenericLine.__init__(self, **kwargs)
+
+
+    def __repr__(self):
+        return "<Path object: %s points>" % len(self.points)
+
+
+
+class Rectangle(GenericBox, GenericShape):
+
+    def __init__(self, *args, **kwargs):
+        GenericBox.__init__(self, *args)
+        GenericShape.__init__(self, **kwargs)
+
+
+    def __repr__(self):
+        return "<%i × %i Rectangle object at (%i,%i)>" % (
+         self.width, self.height, self.x, self.y
+        )
+
+
+
+class Polygon(GenericShape):
     pass
 
 
 
-class Rectangle(GenericBox, GenericLine, GenericShape):
+class Oval(GenericBox, GenericShape):
     pass
 
 
 
-class Polygon(GenericLine, GenericShape):
-    pass
-
-
-
-class Oval(GenericBox, GenericLine, GenericShape):
-    pass
-
-
-
-class Arc(GenericBox, GenericLine, GenericShape):
+class Arc(GenericBox, GenericShape):
     pass
 
 
---
+++ tests/test_graphics.py
@@ -0,0 +1,84 @@
+import unittest
+import sys
+sys.path.append(".")
+from omnicanvas.graphics import *
+
+class GraphicsUnitTest(unittest.TestCase):
+
+    def check_valid_box(self, box):
+        box.x + 1
+        box.y + 1
+        box.width + 1
+        box.height + 1
+
+
+    def check_valid_line(self, line):
+        self.assertIsInstance(line.line_style, str)
+        self.assertIsInstance(line.line_width, int)
+        self.assertIsInstance(line.line_color, str)
+        self.assertEqual(len(line.line_color), 7)
+
+
+    def check_valid_shape(self, shape):
+        self.check_valid_line(shape)
+        if shape.fill_color is not None:
+            self.assertIsInstance(shape.fill_color, str)
+            self.assertEqual(len(shape.shape_color), 7)
+        self.assertGreaterEqual(shape.opacity, 0)
+        self.assertLessEqual(shape.opacity, 1)
+
+
+
+class TestLineGraphic(GraphicsUnitTest):
+
+    def test_doesnt_need_keyword_arguments(self):
+        line = Line(1, 1, 2, 2)
+        self.check_valid_line(line)
+
+
+    def test_will_accept_keywords(self):
+        line = Line(1, 1, 2, 2, line_style="-", line_width=1, line_color="#000000")
+        self.check_valid_line(line)
+
+
+
+class TestPathGraphic(GraphicsUnitTest):
+
+    def test_path_points_format_correctly(self):
+        path = Path(10,10, 10,30, 34,45, 100,101, 19,200)
+        self.check_valid_line(path)
+        self.assertIsInstance(path.points, list)
+        for point in path.points:
+            self.assertIsInstance(point, tuple)
+            self.assertEqual(len(point), 2)
+
+
+    def test_cant_provide_odd_number_of_points(self):
+        self.assertRaises(AssertionError, lambda: Path(1, 2, 3, 4, 5))
+
+
+
+class TestRectangleGraphic(GraphicsUnitTest):
+
+    def test_can_create_rectangle(self):
+        rectangle = Rectangle(1, 1, 10, 10)
+        self.check_valid_box(rectangle)
+        self.check_valid_shape(rectangle)
+
+
+
+class TestValidRepr(GraphicsUnitTest):
+
+    def test_reprs_ok(self):
+        line = Line(1,1,2,2)
+        path = Path(1,1,2,2,3,3)
+        rectangle = Rectangle(1,1,2,2)
+
+        self.assertRegex(line.__repr__(), r"<Line object: (.+,.+) to (.+,.+)>")
+        self.assertRegex(path.__repr__(), r"<Path object: .+ points>")
+        self.assertRegex(rectangle.__repr__(), r"<.+ × .+ Rectangle object at (.+,.+)>")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
