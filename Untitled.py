eaec5c7b3a8ff92dd50ad0bd6c50ac130f793bfe
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-13 10:24:51 +0100

    Remove temp file

--- Untitled.py
+++
@@ -1,7 +0,0 @@
-import omnicanvas
-canvas = omnicanvas.Canvas(500, 300, background_color="#A8CCF0")
-canvas.draw_rectangle(10, 10, 200, 200, fill_color="#D92626", line_width=0)
-canvas.draw_rectangle(290, 90, 200, 200, fill_color="#144BB8", line_width=0)
-canvas.draw_oval(150, 50, 200, 200, fill_color="#00FF00", opacity=0.4)
-
-canvas.save("svg", "docs/source/example.svg")
