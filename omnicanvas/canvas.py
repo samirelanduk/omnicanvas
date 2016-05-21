319cfd8dcd86b7b718abe1ea07c0cd35bc2460ca
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-21 12:56:56 +0100

    Add float conversion to Canvas dimensions

--- omnicanvas/canvas.py
+++ omnicanvas/canvas.py
@@ -1,14 +1,18 @@
 class Canvas:
 
     def __init__(self, width, height):
+        if isinstance(width, float):
+            width = round(width)
         if not isinstance(width, int):
             raise TypeError("Width must be numeric, not '%s'" % width)
         self.width = width
 
+        if isinstance(height, float):
+            height = round(height)
         if not isinstance(height, int):
             raise TypeError("Height must be numeric, not '%s'" % height)
         self.height = height
-        
+
         self.graphics = []
 
 
--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -23,6 +23,23 @@ class CanvasCreationTests(TestCase):
             Canvas("700", "500")
 
 
+    def test_float_dimensions_converted_to_int(self):
+        canvas = Canvas(100.0, 200)
+        self.assertEqual(canvas.width, 100)
+        self.assertIsInstance(canvas.width, int)
+        canvas = Canvas(100.1, 200)
+        self.assertEqual(canvas.width, 100)
+        canvas = Canvas(100.6, 200)
+        self.assertEqual(canvas.width, 101)
+        canvas = Canvas(100, 200.0)
+        self.assertEqual(canvas.height, 200)
+        self.assertIsInstance(canvas.height, int)
+        canvas = Canvas(100, 200.1)
+        self.assertEqual(canvas.height, 200)
+        canvas = Canvas(100, 200.6)
+        self.assertEqual(canvas.height, 201)
+
+
 
 if __name__ == "__main__":
     main()
