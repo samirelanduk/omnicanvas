245b82eba7f248b674017d3e1d4da0d5130d1dca
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-26 17:09:53 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+ name="omnicanvas",
+ version="0.1.0",
+ description="A Python generic canvas which can paint itselg to SVG etc.",
+ url="https://omnicanvas.readthedocs.io",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Other Audience",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Multimedia :: Graphics",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.0",
+  "Programming Language :: Python :: 3.1",
+  "Programming Language :: Python :: 3.2",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+ ],
+ keywords="svg graphics canvas diagrams art",
+ packages=["omnicanvas"],
+ install_requires=[]
+)
