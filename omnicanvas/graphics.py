6fb45bdab7a50640a8d439827fdc9c6fdf0ead3f
Sam Ireland <sam.ireland.uk@gmail.com>
2016-12-09 23:26:55 +0000

    Hide BoxGraphic attributes

--- omnicanvas/graphics.py
+++ omnicanvas/graphics.py
@@ -302,19 +302,19 @@ class BoxGraphic(ShapeGraphic):
 
         if not isinstance(x, int) and not isinstance(x, float):
             raise TypeError("x must be numeric, not '%s'" % x)
-        self.x = x
+        self._x = x
 
         if not isinstance(y, int) and not isinstance(y, float):
             raise TypeError("y must be numeric, not '%s'" % y)
-        self.y = y
+        self._y = y
 
         if not isinstance(width, int) and not isinstance(width, float):
             raise TypeError("width must be numeric, not '%s'" % width)
-        self.width = width
+        self._width = width
 
         if not isinstance(height, int) and not isinstance(height, float):
             raise TypeError("height must be numeric, not '%s'" % height)
-        self.height = height
+        self._height = height
 
 
 
--- tests/test_boxes.py
+++ tests/test_boxes.py
@@ -6,32 +6,32 @@ class BoxGraphicCreationTests(TestCase):
     def test_can_create_box_graphic(self):
         box = BoxGraphic(10, 20, 100, 200)
         self.assertIsInstance(box, ShapeGraphic)
-        self.assertEqual(box.x, 10)
-        self.assertEqual(box.y, 20)
-        self.assertEqual(box.width, 100)
-        self.assertEqual(box.height, 200)
-        self.assertEqual(box.fill_color, "#FFFFFF")
-        self.assertEqual(box.opacity, 1)
-        self.assertEqual(box.line_width, 1)
-        self.assertEqual(box.line_style, "-")
-        self.assertEqual(box.line_color, "#000000")
-        self.assertEqual(box.rotation, (0, 0, 0))
-        self.assertEqual(box.data, {})
+        self.assertEqual(box._x, 10)
+        self.assertEqual(box._y, 20)
+        self.assertEqual(box._width, 100)
+        self.assertEqual(box._height, 200)
+        self.assertEqual(box._fill_color, "#FFFFFF")
+        self.assertEqual(box._opacity, 1)
+        self.assertEqual(box._line_width, 1)
+        self.assertEqual(box._line_style, "-")
+        self.assertEqual(box._line_color, "#000000")
+        self.assertEqual(box._rotation, (0, 0, 0))
+        self.assertEqual(box._data, {})
 
 
     def test_box_location_must_be_numeric(self):
         with self.assertRaises(TypeError):
-            box = BoxGraphic("10", 20, 100, 200)
+            BoxGraphic("10", 20, 100, 200)
         with self.assertRaises(TypeError):
-            box = BoxGraphic(10, "20", 100, 200)
-        box = BoxGraphic(10.5, 20, 100, 200)
-        box = BoxGraphic(10, 20.5, 100, 200)
+            BoxGraphic(10, "20", 100, 200)
+        BoxGraphic(10.5, 20, 100, 200)
+        BoxGraphic(10, 20.5, 100, 200)
 
 
     def test_box_dimensions_must_be_numeric(self):
         with self.assertRaises(TypeError):
-            box = BoxGraphic(10, 20, "100", 200)
+            BoxGraphic(10, 20, "100", 200)
         with self.assertRaises(TypeError):
-            box = BoxGraphic(10, 20, 100, "200")
-        box = BoxGraphic(10, 20, 100.5, 200)
-        box = BoxGraphic(10, 20, 100, 200.5)
+            BoxGraphic(10, 20, 100, "200")
+        BoxGraphic(10, 20, 100.5, 200)
+        BoxGraphic(10, 20, 100, 200.5)
