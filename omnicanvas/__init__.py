7118c3db6855bc545b8bca70a5078f03ef911ad0
Sam Ireland <sam.ireland.uk@gmail.com>
2016-12-14 20:02:03 +0000

    Start 0.2.1

--- omnicanvas/__init__.py
+++ omnicanvas/__init__.py
@@ -1,5 +1,5 @@
 from .canvas import Canvas
 from .colors import hsl_to_rgb
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Sam Ireland"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="omnicanvas",
- version="0.2.0",
+ version="0.2.1",
  description="A Python generic canvas which can paint itself to SVG etc.",
  url="https://omnicanvas.readthedocs.io",
  author="Sam Ireland",
